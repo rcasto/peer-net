df37f520cc975a61e815006964caea3bd05c2c4e
Richie <rcasto92@gmail.com>
2017-11-07 21:32:57 -0800

    reorganize some of the server logic into modules

--- server.js
+++ server.js
@@ -1,25 +1,12 @@
-var express = require('express');
-var path = require('path');
-var ws = require('ws');
 var http = require('http');
+var app = require('./lib/app');
+var socketServerGenerator = require('./lib/socketServerGenerator');
 
 var port = process.env.port || 3000;
-var app = express();
 var server = http.createServer(app);
-var socketServer = new ws.Server({ server });
-
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.get('/', (req, res) => {
-    res.sendFile('index.html');
+var socketServer = socketServerGenerator({
+    clientTracking: true,
+    server
 });
 
 server.listen(port, () => console.log(`Server started on port ${port}`));
-
-socketServer.on('connection', (socket, req) => {
-    console.log('Client connected');
-    socket.on('message', message => {
-        console.log(`Received: ${message}`);
-    });
-    socket.send('something');
-});
